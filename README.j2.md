772ac1faa570be72afde9add4ff03f7df185c792
Emanuele Plebani <banus80@gmail.com>
2017-08-06 19:31:58 +0200

    generate README.md and cumulative plot from tsv, auto-update plot range
    The README is generated from gans.tsv suing the Jinja2 template engine;
    entry ordering changed because it now follows unicode ordering.
    Cumulative plot generated from the .tsv by using the Year and Month
    fields; range automatically updated to the next 10s.

---
+++ README.j2.md
@@ -0,0 +1,23 @@
+# The GAN Zoo
+
+<p align="center"><img width="40%" src="The_GAN_Zoo.jpg" /></p>
+
+Every week, new GAN papers are coming out and it's hard to keep track of them all, not to mention the incredibly creative ways in which researchers are naming these GANs! So, here's a list of what started as a fun activity compiling all named GANs!
+
+<p align="center"><img width="50%" src="cumulative_gans.jpg" /></p>
+
+You can also check out the same data in a tabular format with functionality to filter by year or do a quick search by title [here](https://github.com/hindupuravinash/the-gan-zoo/blob/master/gans.tsv).
+
+Contributions are welcome. Add links through pull requests or create an issue to lemme know something I missed or to start a discussion.
+
+Check out [Deep Hunt](https://deephunt.in) - my weekly AI newsletter for this repo as [blogpost](https://medium.com/deep-hunt/the-gan-zoo-79597dc8c347) and follow me on [Twitter](https://www.twitter.com/hindupuravinash).
+
+{% for gan in gans %}
+* {{ gan['Abbr.'] }} - [{{ gan['Title'] }}]({{ gan['Arxiv'] }})
+  {%- if gan['Official_Code'] != '-' -%} 
+  {#- #} ([github]({{ gan['Official_Code'] }}))
+  {% else %} {# space removed if no github repository #}
+
+  {% endif %}
+{%- endfor %}
+
---
+++ update.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+""" Update Readme.md and cumulative_gans.jpg """
+from __future__ import print_function
+from __future__ import division
+
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns   # implicitly use seaborn theme in matplotlib
+
+
+def load_data():
+    """ Load GANs data from the gans.csv file """
+    import csv
+
+    with open('gans.tsv') as fid:
+        reader = csv.DictReader(fid, delimiter='\t')
+        gans = [row for row in reader]
+    return gans
+
+
+def update_readme(gans):
+    """ Update the Readme.md text file from a Jinja2 template """
+    import jinja2 as j2
+
+    gans.sort(key=lambda v: v['Abbr.'].upper())
+    j2_env = j2.Environment(loader=j2.FileSystemLoader('.'),
+                            trim_blocks=True, lstrip_blocks=True)
+
+    with open('README.md', 'w') as fid:
+        print(j2_env.get_template('README.j2.md').render(gans=gans), file=fid)
+
+
+def update_figure(gans):
+    """ Update the figure cumulative_gans.jpg """
+    data = np.array([int(gan['Year']) + int(gan['Month']) / 12
+                     for gan in gans])
+    y_range = int(np.ceil(data.size / 10)) * 10
+
+    plt.hist(data, 100, cumulative="True")
+    plt.xticks(range(2014, 2018))
+    plt.yticks(np.arange(0, y_range, 10))
+    plt.title("Cumulative number of named GAN papers by month")
+    plt.xlabel("Year")
+    plt.ylabel("Total number of papers")
+    plt.savefig('cumulative_gans.jpg')
+
+
+if __name__ == '__main__':
+    GANS = load_data()
+    update_readme(GANS)
+    update_figure(GANS)
